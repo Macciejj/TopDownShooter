4c09ff3d5cab52dc42b4302195d9c63657c52cfe
Macciejj <macios99@gmail.com>
2022-09-08 22:47:31 +0200

    shadows and navmesh2d

--- Shooter/Assets/Scripts/EnemyAI.cs
+++ Shooter/Assets/Scripts/EnemyAI.cs
@@ -1,43 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
     [SerializeField] float seeingRange = 5;
-    [SerializeField] Transform player;
+    [SerializeField] GameObject player;
     [SerializeField] float angle = 30;
+    NavMeshAgent navMesh;
     void Start()
     {
-        
+        navMesh = GetComponent<NavMeshAgent>();
+        navMesh.updateUpAxis = false;
     }
 
     void Update()
     {
         if(GetDistanceToPlayer() < seeingRange)
         {
-            print("i see player");
+            SetDestination(player);
         }
     }
-    
+    void SetDestination(GameObject target)
+    {
+        var agentDrift = 0.0001f; // minimal
+        var driftPos = target.transform.position + (Vector3)(agentDrift * Random.insideUnitCircle);
+        navMesh.SetDestination(driftPos);
+    }
     private void OnDrawGizmos()
     {
-        float angleInRadians = angle * Mathf.PI / 180;
-
         Gizmos.DrawWireSphere(transform.position, seeingRange);
+        //float angleInRadians = angle * Mathf.PI / 180 + transform.rotation.z;
 
-        Gizmos.color = Color.green;
-        float xEndOfRadiusPosition = -seeingRange * Mathf.Cos(angleInRadians) + transform.position.x;
-        float yEndOfRadiusPosition = -seeingRange * Mathf.Sin(angleInRadians) + transform.position.y;
-        Gizmos.DrawLine(transform.position, new Vector3(xEndOfRadiusPosition, yEndOfRadiusPosition, 0));
+        
 
-        float xEndOfRadiusPosition2 = seeingRange * Mathf.Cos(-angleInRadians) + transform.position.x;
-        float yEndOfRadiusPosition2 = seeingRange * Mathf.Sin(-angleInRadians) + transform.position.y;
-        Gizmos.DrawLine(transform.position, new Vector3(xEndOfRadiusPosition2, yEndOfRadiusPosition2, 0));
+        //Gizmos.color = Color.green;
+        //float xEndOfRadiusPosition = -seeingRange * Mathf.Cos(angleInRadians) + transform.position.x;
+        //float yEndOfRadiusPosition = -seeingRange * Mathf.Sin(angleInRadians) + transform.position.y;
+        //Gizmos.DrawLine(transform.position, new Vector3(xEndOfRadiusPosition, yEndOfRadiusPosition, 0));
+
+        //float xEndOfRadiusPosition2 = seeingRange * Mathf.Cos(-angleInRadians) + transform.position.x;
+        //float yEndOfRadiusPosition2 = seeingRange * Mathf.Sin(-angleInRadians) + transform.position.y;
+        //Gizmos.DrawLine(transform.position, new Vector3(xEndOfRadiusPosition2, yEndOfRadiusPosition2, 0));
+
+        
     }
     private float GetDistanceToPlayer()
     {
-        return Vector2.Distance(transform.position, player.position);
+        return Vector2.Distance(transform.position, player.transform.position);
     }
 
     private void Chase()
